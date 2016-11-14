ee8f2e3cf8d89c453b1b54a23b38dae79cb58bcd
user 08 <user_08@mac-08deimac.local>
2016-11-14 17:59:41 +0800

    Initial Commit

---
+++ hw1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by user_08 on 2016/11/14.
+//  Copyright © 2016年 user_08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
